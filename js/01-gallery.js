8f54a9c2484d25ef5928a95e4d04161b5b199e70
nadiyniy <140396602+nadiyniy@users.noreply.github.com>
2023-09-19 17:15:46 +0300

    finish hw7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const listEl = document.querySelector(".gallery");
+const galleryImage = galleryItems
+  .map(
+    ({ original, preview, description }) =>
+      `<li class="gallery__item">
+      <a class="gallery__link" href="${original}" >
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li>`
+  )
+  .join("");
+
+listEl.insertAdjacentHTML("beforeend", galleryImage);
+
+let modal = null;
+
+const openPic = (e) => {
+  e.preventDefault();
+
+  if (e.target.nodeName !== "IMG") return;
+
+  modal = basicLightbox.create(
+    `<img src="${e.target.dataset.source}" width="800" height="600">`
+  );
+
+  modal.show();
+  document.addEventListener("keydown", onEscapeClick);
+};
+
+const onEscapeClick = (e) => {
+  if (e.code !== "Escape") return;
+  
+  modal.close();
+  document.removeEventListener("keydown", onEscapeClick);
+};
+
+listEl.addEventListener("click", openPic);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+const listEl = document.querySelector(".gallery");
+
+const galleryImage = galleryItems
+  .map(
+    ({ original, preview, description }) =>
+      `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`
+  )
+  .join("");
+
+listEl.insertAdjacentHTML("beforeend", galleryImage);
+
+let lightbox = new SimpleLightbox('.gallery a', { 
+    captionsData: 'alt',
+    captionDelay: 250,
+ });
+
